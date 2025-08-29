14cf37c0e33318033664bfc8e89518405a1f85a2
Tanmay Dalavi <tanmay.dalavi@onextel.com>
2025-08-29 12:47:54 +0530

    Delete: Day_21 dir

--- Day_21/bookmanagement/src/main/java/org/example/bookmanagement/repository/PriceRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.bookmanagement.repository;
-
-import org.example.bookmanagement.entity.PriceEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PriceRepository extends JpaRepository<PriceEntity, Long> {
-}
--- Day_21/bookmanagement/src/main/java/org/example/bookmanagement/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.bookmanagement.repository;
-
-import org.example.bookmanagement.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-}
